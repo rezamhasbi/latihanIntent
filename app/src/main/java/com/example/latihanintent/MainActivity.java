ed1258aa3b24b5fbe8a323e31efbeff36e241fcb
rezamhasbi <rezamhasbi98@gmail.com>
2020-02-16 15:52:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanintent/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.latihanintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+         btnMoveActivity.setOnClickListener(this);
+
+         Button btnMoveWithData = findViewById(R.id.btn_move_activity_data);
+         btnMoveWithData.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+            break;
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME,"pler");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,5);
+                startActivity(moveWithDataIntent);
+                break;
+        }
+    }
+}
